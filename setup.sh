f2111e8f14bc898d20dd5cfe7e4c94d16ea3c864
savourylie <calvin.j.ku@googlemail.com>
2020-07-02 18:20:58 +0800

    Fixed syntax error.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
-echo “\
+echo "\
 [general]\n\
-email = \”calvin.j.ku@googlemail.com\”\n\
-“ > ~/.streamlit/credentials.toml
-echo “\
+email = \"calvin.j.ku@googlemail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-“ > ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml
